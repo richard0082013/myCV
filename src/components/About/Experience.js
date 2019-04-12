682d2150628bc1570b1a3acda0e9abaa9387abd1
rtian <rtian@twopicode.com.au>
2019-04-12 12:44:45 +1000

    update experience

--- src/components/About/Experience.js
+++ src/components/About/Experience.js
@@ -3,6 +3,7 @@ import { Image, Item } from "semantic-ui-react";
 import barclaysLogo from "../../assets/image/barclays.png";
 import lendiLogo from "../../assets/image/lendi.png";
 import twopiLogo from "../../assets/image/twopi.png";
+import AITLogo from "../../assets/image/noImage.png";
 class Experience extends Component {
   render() {
     return (
@@ -12,12 +13,13 @@ class Experience extends Component {
 
           <Item.Content>
             <Item.Header as="a">Front-end Web Developer</Item.Header>
+            <Item.Meta>TwoPi Code (Sydney)</Item.Meta>
             <Item.Meta>Jul 2018 - now</Item.Meta>
             <Item.Description>
               <Image src="https://react.semantic-ui.com/images/wireframe/short-paragraph.png" />
             </Item.Description>
             <Item.Extra>
-              Key words: HTML, CSS, ReactJS, Redux, Ant-Design
+              Key words: HTML, CSS, ReactJS, Redux, Ant-Design, python
             </Item.Extra>
           </Item.Content>
         </Item>
@@ -27,6 +29,7 @@ class Experience extends Component {
 
           <Item.Content>
             <Item.Header as="a">Full Stack Web Developer</Item.Header>
+            <Item.Meta>Lendi Pty Ltd (Sydney)</Item.Meta>
             <Item.Meta>Jan 2017 - Jun 2018</Item.Meta>
             <Item.Description>
               <Image src="https://react.semantic-ui.com/images/wireframe/short-paragraph.png" />
@@ -38,11 +41,26 @@ class Experience extends Component {
         </Item>
 
         <Item>
+          <Item.Image size="tiny" src={AITLogo} />
+
+          <Item.Content>
+            <Item.Header as="a">Junior Web Developer</Item.Header>
+            <Item.Meta>AIT Software Development company (ShangHai)</Item.Meta>
+            <Item.Meta>Jan 2014 - Sep 2016</Item.Meta>
+            <Item.Description>
+              <Image src="https://react.semantic-ui.com/images/wireframe/short-paragraph.png" />
+            </Item.Description>
+            <Item.Extra>Key words: HTML, CSS, TDD</Item.Extra>
+          </Item.Content>
+        </Item>
+
+        <Item>
           <Item.Image size="tiny" src={barclaysLogo} />
 
           <Item.Content>
             <Item.Header as="a">Technical Support</Item.Header>
-            <Item.Meta>Sep 2009 - Mar 2012 </Item.Meta>
+            <Item.Meta>BTCC (ShangHai)</Item.Meta>
+            <Item.Meta>Sep 2009 - OCT 2013 </Item.Meta>
             <Item.Description>
               <Image
                 src={
